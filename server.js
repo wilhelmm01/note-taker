283cad89edb97460ea87873be82f8892c5a923f7
Your Name <matt14w@gmail.com>
2022-01-23 15:07:59 -0500

    add heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const allNotes = require('./db/db.json');
+const allNotes = require('./Develop/db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,15 +16,15 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 function createNewNote(body, notesArray) {
@@ -40,7 +40,7 @@ function createNewNote(body, notesArray) {
 
     notesArray.push(newNote);
     fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
+        path.join(__dirname, './Develop/db/db.json'),
         JSON.stringify(notesArray, null, 2)
     );
     return newNote;
@@ -58,7 +58,7 @@ function deleteNote(id, notesArray) {
         if (note.id == id) {
             notesArray.splice(i, 1);
             fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
+                path.join(__dirname, './Develop/db/db.json'),
                 JSON.stringify(notesArray, null, 2)
             );
 
