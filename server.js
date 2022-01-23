925e0c353d917b76503244dc9680839d9f938bf8
Your Name <matt14w@gmail.com>
2022-01-23 15:11:29 -0500

    updated server

--- server.js
+++ server.js
@@ -1,77 +1,77 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
+// const PORT = process.env.PORT || 3001;
+// const fs = require('fs');
+// const path = require('path');
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const allNotes = require('./Develop/db/db.json')
+// const allNotes = require('./Develop/db/db.json')
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
+// app.get('/api/notes', (req, res) => {
+//     res.json(allNotes.slice(1));
+// });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+// });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-});
+// app.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+// });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+// });
 
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
+// function createNewNote(body, notesArray) {
+//     const newNote = body;
+//     if (!Array.isArray(notesArray))
+//         notesArray = [];
     
-    if (notesArray.length === 0)
-        notesArray.push(0);
+//     if (notesArray.length === 0)
+//         notesArray.push(0);
 
-    body.id = notesArray[0];
-    notesArray[0]++;
+//     body.id = notesArray[0];
+//     notesArray[0]++;
 
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './Develop/db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
+//     notesArray.push(newNote);
+//     fs.writeFileSync(
+//         path.join(__dirname, './Develop/db/db.json'),
+//         JSON.stringify(notesArray, null, 2)
+//     );
+//     return newNote;
+// }
 
-app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
+// app.post('/api/notes', (req, res) => {
+//     const newNote = createNewNote(req.body, allNotes);
+//     res.json(newNote);
+// });
 
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
+// function deleteNote(id, notesArray) {
+//     for (let i = 0; i < notesArray.length; i++) {
+//         let note = notesArray[i];
 
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './Develop/db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
+//         if (note.id == id) {
+//             notesArray.splice(i, 1);
+//             fs.writeFileSync(
+//                 path.join(__dirname, './Develop/db/db.json'),
+//                 JSON.stringify(notesArray, null, 2)
+//             );
 
-            break;
-        }
-    }
-}
+//             break;
+//         }
+//     }
+// }
 
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
+// app.delete('/api/notes/:id', (req, res) => {
+//     deleteNote(req.params.id, allNotes);
+//     res.json(true);
+// });
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+// app.listen(PORT, () => {
+//     console.log(`API server now on port ${PORT}!`);
+// });
