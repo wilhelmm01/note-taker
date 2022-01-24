5514975da19ef3cfdc0fb84e69871cb85aae1884
Your Name <matt14w@gmail.com>
2022-01-23 19:38:48 -0500

    final commit

--- server.js
+++ server.js
@@ -27,45 +27,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
-// function createNewNote(body, notesArray) {
-//     const newNote = body;
-//     if (!Array.isArray(notesArray))
-//         notesArray = [];
-    
-//     if (notesArray.length === 0)
-//         notesArray.push(0);
 
-//     body.id = notesArray[0];
-//     notesArray[0]++;
-
-//     notesArray.push(newNote);
-//     fs.writeFileSync(
-//         path.join(__dirname, './Develop/db/db.json'),
-//         JSON.stringify(notesArray, null, 2)
-//     );
-//     return newNote;
-// }
 
 app.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
 });
 
-// function deleteNote(id, notesArray) {
-//     for (let i = 0; i < notesArray.length; i++) {
-//         let note = notesArray[i];
 
-//         if (note.id == id) {
-//             notesArray.splice(i, 1);
-//             fs.writeFileSync(
-//                 path.join(__dirname, './Develop/db/db.json'),
-//                 JSON.stringify(notesArray, null, 2)
-//             );
-
-//             break;
-//         }
-//     }
-// }
 
 app.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, allNotes);
